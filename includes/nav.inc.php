488e64c4a340de3e732edf546af9c5ceb6f894b1
Georgi Georgiev <joro.dev@gmail.com>
2017-08-29 17:56:10 +0300

    Added navigation

---
+++ includes/nav.inc.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/css/reset.css">
+        <link rel="stylesheet" href="assets/css/index.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    </head>
+    <body>
+        <div class="main-nav-wrapper">
+            <nav class="main-nav container">
+                <a class="main-nav--logo" href="index.php">Blog.com</a>
+                <ul class="main-nav--menu">
+                    <li><a class="active" href="index.php">Home</a></li>
+                    <li><a href="#">Articles</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <ul class="main-nav--login">
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Register</a></li>
+                </ul>
+            </nav>
+        </div>
