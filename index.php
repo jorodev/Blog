e53336ed67c29ac9266d14c731266558637be5ed
Georgi Georgiev <joro.dev@gmail.com>
2017-08-30 11:29:59 +0300

    Added main section with recent articles on home page

--- index.php
+++ index.php
@@ -1,5 +1,64 @@
 <?php include 'includes/nav.inc.php' ?>
 
-    
+        <div class="home-main--wrapper">
+            <main class="home-main container">
+                <h2 class="home-main--heading">Recent Articles</h2>
+                <hr class="home-main-hr">
+                <article class="home-main--article">
+                    <img class="article-image" src="http://via.placeholder.com/300x150" alt="Image coudn't load">
+                    <div class="article-group">
+                        <h3 class="article-title"3>Some really big title here to test it out for bugs</h3>
+                        <span class="article-author">by <a href="#">Username</a></span>
+                        <p class="article-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit et incidunt, sapiente nisi molestias provident totam magnam rem delectus nesciunt culpa ab quam, fuga ut laudantium quis dolores voluptatibus mollitia.</p>
+                        <a class="article-button" href="#">Read more</a><br /><br />
+                    </div>
+                </article>
+                <article class="home-main--article">
+                    <img class="article-image" src="http://via.placeholder.com/300x150" alt="Image coudn't load">
+                    <div class="article-group">
+                        <h3 class="article-title"3>Some really big title here to test it out for bugs</h3>
+                        <span class="article-author">by <a href="#">Username</a></span>
+                        <p class="article-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit et incidunt, sapiente nisi molestias provident totam magnam rem delectus nesciunt culpa ab quam, fuga ut laudantium quis dolores voluptatibus mollitia.</p>
+                        <a class="article-button" href="#">Read more</a><br /><br />
+                    </div>
+                </article>
+                <article class="home-main--article">
+                    <img class="article-image" src="http://via.placeholder.com/300x150" alt="Image coudn't load">
+                    <div class="article-group">
+                        <h3 class="article-title"3>Some really big title here to test it out for bugs</h3>
+                        <span class="article-author">by <a href="#">Username</a></span>
+                        <p class="article-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit et incidunt, sapiente nisi molestias provident totam magnam rem delectus nesciunt culpa ab quam, fuga ut laudantium quis dolores voluptatibus mollitia.</p>
+                        <a class="article-button" href="#">Read more</a><br /><br />
+                    </div>
+                </article>
+                <article class="home-main--article">
+                    <img class="article-image" src="http://via.placeholder.com/300x150" alt="Image coudn't load">
+                    <div class="article-group">
+                        <h3 class="article-title"3>Some really big title here to test it out for bugs</h3>
+                        <span class="article-author">by <a href="#">Username</a></span>
+                        <p class="article-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit et incidunt, sapiente nisi molestias provident totam magnam rem delectus nesciunt culpa ab quam, fuga ut laudantium quis dolores voluptatibus mollitia.</p>
+                        <a class="article-button" href="#">Read more</a><br /><br />
+                    </div>
+                </article>
+                <article class="home-main--article">
+                    <img class="article-image" src="http://via.placeholder.com/300x150" alt="Image coudn't load">
+                    <div class="article-group">
+                        <h3 class="article-title"3>Some really big title here to test it out for bugs</h3>
+                        <span class="article-author">by <a href="#">Username</a></span>
+                        <p class="article-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit et incidunt, sapiente nisi molestias provident totam magnam rem delectus nesciunt culpa ab quam, fuga ut laudantium quis dolores voluptatibus mollitia.</p>
+                        <a class="article-button" href="#">Read more</a><br /><br />
+                    </div>
+                </article>
+                <article class="home-main--article">
+                    <img class="article-image" src="http://via.placeholder.com/300x150" alt="Image coudn't load">
+                    <div class="article-group">
+                        <h3 class="article-title"3>Some really big title here to test it out for bugs</h3>
+                        <span class="article-author">by <a href="#">Username</a></span>
+                        <p class="article-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit et incidunt, sapiente nisi molestias provident totam magnam rem delectus nesciunt culpa ab quam, fuga ut laudantium quis dolores voluptatibus mollitia.</p>
+                        <a class="article-button" href="#">Read more</a><br /><br />
+                    </div>
+                </article>
+            </main>
+        </div>
     </body>
 </html>
