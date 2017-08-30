0a7c2c3df30c830b50e5791c286da65a0bf1dc26
Georgi Georgiev <joro.dev@gmail.com>
2017-08-30 12:06:23 +0300

    Changed date format in articles on home page

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
                     <img class="article-image" src="http://via.placeholder.com/300x150" alt="Image coudn't load">
                     <div class="article-group">
                         <h3 class="article-title"3>Some really big title here to test it out for bugs</h3>
-                        <div class="article-date">Posted <span><?php echo date('d/m/y h:i') ?></span></div>
+                        <div class="article-date">Posted <span><?php echo date('d.m.y h:i') ?></span></div>
                         <div class="article-author">by <a href="#">Username</a></div>
                         <p class="article-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit et incidunt, sapiente nisi molestias provident totam magnam rem delectus nesciunt culpa ab quam, fuga ut laudantium quis dolores voluptatibus mollitia.</p>
                         <a class="article-button" href="#">Read more</a><br /><br />
@@ -18,7 +18,7 @@
                     <img class="article-image" src="http://via.placeholder.com/300x150" alt="Image coudn't load">
                     <div class="article-group">
                         <h3 class="article-title"3>Some really big title here to test it out for bugs</h3>
-                        <div class="article-date">Posted <span><?php echo date('d/m/y h:i') ?></span></div>
+                        <div class="article-date">Posted <span><?php echo date('d.m.y h:i') ?></span></div>
                         <div class="article-author">by <a href="#">Username</a></div>
                         <p class="article-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit et incidunt, sapiente nisi molestias provident totam magnam rem delectus nesciunt culpa ab quam, fuga ut laudantium quis dolores voluptatibus mollitia.</p>
                         <a class="article-button" href="#">Read more</a><br /><br />
@@ -28,7 +28,7 @@
                     <img class="article-image" src="http://via.placeholder.com/300x150" alt="Image coudn't load">
                     <div class="article-group">
                         <h3 class="article-title"3>Some really big title here to test it out for bugs</h3>
-                        <div class="article-date">Posted <span><?php echo date('d/m/y h:i') ?></span></div>
+                        <div class="article-date">Posted <span><?php echo date('d.m.y h:i') ?></span></div>
                         <div class="article-author">by <a href="#">Username</a></div>
                         <p class="article-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit et incidunt, sapiente nisi molestias provident totam magnam rem delectus nesciunt culpa ab quam, fuga ut laudantium quis dolores voluptatibus mollitia.</p>
                         <a class="article-button" href="#">Read more</a><br /><br />
@@ -38,7 +38,7 @@
                     <img class="article-image" src="http://via.placeholder.com/300x150" alt="Image coudn't load">
                     <div class="article-group">
                         <h3 class="article-title"3>Some really big title here to test it out for bugs</h3>
-                        <div class="article-date">Posted <span><?php echo date('d/m/y h:i') ?></span></div>
+                        <div class="article-date">Posted <span><?php echo date('d.m.y h:i') ?></span></div>
                         <div class="article-author">by <a href="#">Username</a></div>
                         <p class="article-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit et incidunt, sapiente nisi molestias provident totam magnam rem delectus nesciunt culpa ab quam, fuga ut laudantium quis dolores voluptatibus mollitia.</p>
                         <a class="article-button" href="#">Read more</a><br /><br />
@@ -48,7 +48,7 @@
                     <img class="article-image" src="http://via.placeholder.com/300x150" alt="Image coudn't load">
                     <div class="article-group">
                         <h3 class="article-title"3>Some really big title here to test it out for bugs</h3>
-                        <div class="article-date">Posted <span><?php echo date('d/m/y h:i') ?></span></div>
+                        <div class="article-date">Posted <span><?php echo date('d.m.y h:i') ?></span></div>
                         <div class="article-author">by <a href="#">Username</a></div>
                         <p class="article-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit et incidunt, sapiente nisi molestias provident totam magnam rem delectus nesciunt culpa ab quam, fuga ut laudantium quis dolores voluptatibus mollitia.</p>
                         <a class="article-button" href="#">Read more</a><br /><br />
@@ -58,7 +58,7 @@
                     <img class="article-image" src="http://via.placeholder.com/300x150" alt="Image coudn't load">
                     <div class="article-group">
                         <h3 class="article-title"3>Some really big title here to test it out for bugs</h3>
-                        <div class="article-date">Posted <span><?php echo date('d/m/y h:i') ?></span></div>
+                        <div class="article-date">Posted <span><?php echo date('d.m.y h:i') ?></span></div>
                         <div class="article-author">by <a href="#">Username</a></div>
                         <p class="article-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit et incidunt, sapiente nisi molestias provident totam magnam rem delectus nesciunt culpa ab quam, fuga ut laudantium quis dolores voluptatibus mollitia.</p>
                         <a class="article-button" href="#">Read more</a><br /><br />
