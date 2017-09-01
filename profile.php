3b455c65e2ad5738b1dbd437b44a5a2fff24278b
Georgi Georgiev <joro.dev@gmail.com>
2017-09-01 12:28:15 +0300

    Back-end: Users can now activate their account

--- profile.php
+++ profile.php
@@ -7,10 +7,22 @@
         <strong>Hello, <?php echo $_SESSION['username']; ?>.</strong> <?php echo $_SESSION['success']; unset($_SESSION['success']); ?>
     </div>
     <?php } ?>
+    <?php if (isset($_SESSION['activated'])) { ?>
+    <div class="alert success">
+        <span class="closebtn">&times;</span>  
+        <strong></strong> <?php echo $_SESSION['activated']; unset($_SESSION['activated']); ?>
+    </div>
+    <?php } ?>
     <?php if (isset($_SESSION['verify'])) { ?>
     <div class="alert info">
         <span class="closebtn">&times;</span>  
-        <strong>Warning!</strong> <?php echo $_SESSION['verify']; unset($_SESSION['verify']) ?>
+        <strong></strong> <?php echo $_SESSION['verify']; unset($_SESSION['verify']) ?>
+    </div>
+    <?php } ?>
+    <?php if (isset($_SESSION['error'])) { ?>
+    <div class="alert">
+        <span class="closebtn">&times;</span>  
+        <strong></strong> <?php echo $_SESSION['error']; unset($_SESSION['error']) ?>
     </div>
     <?php } ?>
 </div>
---
+++ verify.php
@@ -0,0 +1,31 @@
+<?php 
+
+require 'db/dbconnection.php';
+session_start();
+
+if (isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])) {
+    $email = $mysqli->escape_string($_GET['email']);
+    $hash = $mysqli->escape_string($_GET['hash']);
+
+    $result = $mysqli->query("SELECT * FROM users WHERE email='$email' AND hash='$hash' AND active='0'");
+
+    if ($result->num_rows == 0) {
+        $_SESSION['error'] = "Account has already been activated or the URL is invalid!";
+
+        header("location: profile.php");
+    } else {
+        $_SESSION['activated'] = "Your account has been activated!";
+
+        $mysqli->query("UPDATE users SET active='1' WHERE email='$email'")
+        or die($mysqli->error);
+
+        $_SESSION['active'] = 1;
+
+        header("location: profile.php");
+    }
+} else {
+    $_SESSION['error'] = "Invalid parameters provided for account verification!";
+    header("location: profile.php");
+}
+
+?>
